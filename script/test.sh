a63b2f64c738d6e3d387c36fbff3863cf6c15c9f
gongbo <gb1932002@aliyun.com>
2018-01-11 19:58:05 +0800

    [gongbo] #N/A add test script

--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
+set -x
+set -e
 
-ls
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean test
