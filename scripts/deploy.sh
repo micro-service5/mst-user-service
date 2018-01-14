b14c02f47ca4f8428d0e84e56d99f4f863c4bd64
xqfgbc <gb1932002@aliyun.com>
2018-01-14 14:15:35 +0800

    [gongbo] #N/A change variable name in build and deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@
 set -x
 set -e
 
-if [[ -z $DOCKER_REGISRTY ]]; then
-  DOCKER_REGISRTY=127.0.0.1:5000
+if [[ -z $DOCKER_REGISTRY ]]; then
+  DOCKER_REGISTRY=127.0.0.1:5000
 fi
-IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
+IMAGE_NAME=${DOCKER_REGISTRY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
 
 docker run -d -p 8080:8080 $IMAGE_NAME
