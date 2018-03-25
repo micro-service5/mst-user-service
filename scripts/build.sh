66d0ede9be7dcdd6a6a235786daa5cc80a3353f7
xqfgbc <gb1932002@aliyun.com>
2018-03-25 21:10:53 +0800

    change docker registry ip

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 
 docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean bootRepackage
 if [[ -z $DOCKER_REGISTRY ]]; then
-  DOCKER_REGISTRY=10.229.5.155:5000
+  DOCKER_REGISTRY=10.29.5.155:5000
 fi
 IMAGE_NAME=${DOCKER_REGISTRY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
 docker build -t $IMAGE_NAME .
